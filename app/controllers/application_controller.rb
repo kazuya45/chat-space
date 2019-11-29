932b80f818685f893d6333ca656c1c9a2c25aaac
kazuya45 <mabou.0915@gmail/com>
2019-11-29 18:05:38 +0900

    ユーザー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+  def after_sign_out_path_for(resource)
+    '/users/sign_in' #サインアウト後はログイン画面に遷移
+  end
 end
